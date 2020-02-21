fc704c1e0a72f3e61afe6f4cf727eb200d24761a
Ankit <salian.ankit@tcs.com>
2020-02-21 20:56:25 +0530

    Refactoring to common CRUD interface. Closes #11

--- pet-clinic-data/src/main/java/com/ankit/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/ankit/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.ankit.sfgpetclinic.services;
 
 import com.ankit.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
