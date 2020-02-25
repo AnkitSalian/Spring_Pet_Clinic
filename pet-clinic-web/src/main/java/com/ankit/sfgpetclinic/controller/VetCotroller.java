c15b44a7ddbf7235dde8f44d31a462e94322c127
Ankit <salian.ankit@tcs.com>
2020-02-25 05:59:27 +0530

    Adding Vets page and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/com/ankit/sfgpetclinic/controller/VetCotroller.java
@@ -0,0 +1,14 @@
+package com.ankit.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetCotroller {
+
+    @RequestMapping({"vets", "vets/index", "vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
