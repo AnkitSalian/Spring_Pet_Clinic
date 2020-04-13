a655ea5c5985fbacb04f14801cf0c0a2205b2bf1
Ankit <salian.ankit@tcs.com>
2020-04-13 11:31:21 +0530

    Fix display owners. Closes #61

--- pet-clinic-web/src/test/java/com/ankit/sfgpetclinic/controller/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/ankit/sfgpetclinic/controller/OwnerControllerTest.java
@@ -6,6 +6,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -70,5 +71,16 @@ class OwnerControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("notImplemented"));
         Mockito.verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void showOwner() throws Exception {
+        Mockito.when(ownerService.findById(ArgumentMatchers.anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("owners/ownerDetails"))
+                .andExpect(MockMvcResultMatchers.model().attribute("owner",
+                        Matchers.hasProperty("id", Matchers.is(1L))));
     }
 }
